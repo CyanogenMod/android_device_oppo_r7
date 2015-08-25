a9bd5886aa68d4ee0c2f91a793195f776cbc83d4
Michael Bestas <mikeioannina@gmail.com>
2015-08-26 01:34:08 +0300

    r7: Initial device tree
    Change-Id: Iaa7e350429a1ac53137cb163a0dad0c16930bd2e

---
+++ cm.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/oppo/r7/device.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+PRODUCT_NAME := cm_r7
+PRODUCT_DEVICE := r7
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := R7f
+PRODUCT_MANUFACTURER := OPPO
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OPPO/R7f/R7f:4.4.4/KTU84P/1427451688:user/release-keys \
+    PRIVATE_BUILD_DESC="msm8916_32-user 4.4.4 KTU84P eng.root.20150626 release-keys"
