50122d9bb2b469f5e3ef2e8585873d7f5e0ce0fa
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-05-06 20:12:07 +0300

    r7: Rename button keylaout
    * Required after kernel changes
    
    Change-Id: Ib0eb6c5e5ff70497f3c13226035c37e1043ab39b

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_COPY_FILES += \
 # Input configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/synaptics-s3508.kl:system/usr/keylayout/synaptics-s3508.kl
+    $(LOCAL_PATH)/keylayout/synaptics-s3508-kpd.kl:system/usr/keylayout/synaptics-s3508-kpd.kl
 
 # Ramdisk
 PRODUCT_PACKAGES += \
