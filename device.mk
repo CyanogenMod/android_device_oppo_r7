58bad1acf9b3403617a16252068e286c75f85a9e
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-17 16:09:03 +0300

    r7: Move GPS HAL back to msm8939-common
    Change-Id: Iedca3dc33d6c98433d9b8eb99d822af39f1f083d

--- device.mk
+++ device.mk
@@ -55,15 +55,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8916
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
 # Input configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
